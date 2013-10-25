6066b253e08331d755cd0bc0e85bbffeccde3c2e
Bob Yexley <bob@yexley.net>
2013-10-25 10:27:33 -0400

    Added a comments section to the post template

--- source/assets/js/index.js
+++ source/assets/js/index.js
@@ -14,6 +14,15 @@
 
         //$("#blog-logo").prependTo("#site-head-content");
 
+        $(".discussion-tabs").on("click", "li", function (e) {
+            var target = $(e.currentTarget);
+            var toShow = $("#" + target.data("show") + "-comments-container");
+            $(".discussion-tabs li").removeClass("active");
+            target.addClass("active");
+            $(".comments-container").addClass("hidden");
+            toShow.removeClass('hidden');
+        });
+
     });
 
 }(jQuery));
