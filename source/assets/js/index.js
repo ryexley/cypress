0d0bbc5ccb9a81860c3556850ea0b7b13f476304
Bob Yexley <bob@yexley.net>
2013-12-14 23:40:42 -0500

    Adding magnific-popup functionality to theme

--- source/assets/js/index.js
+++ source/assets/js/index.js
@@ -7,22 +7,30 @@
 (function ($) {
     "use strict";
 
-    $(document).ready(function(){
-
-        // On the home page, move the blog icon inside the header
-        // for better relative/absolute positioning.
-
-        //$("#blog-logo").prependTo("#site-head-content");
-
-        $(".discussion-tabs").on("click", "li", function (e) {
-            var target = $(e.currentTarget);
-            var toShow = $("#" + target.data("show") + "-comments-container");
-            $(".discussion-tabs li").removeClass("active");
-            target.addClass("active");
-            $(".comments-container").addClass("hidden");
-            toShow.removeClass('hidden');
-        });
-
+    // initialize discussion tabs
+    $(".discussion-tabs").on("click", "li", function (e) {
+        var target = $(e.currentTarget);
+        var toShow = $("#" + target.data("show") + "-comments-container");
+        $(".discussion-tabs li").removeClass("active");
+        target.addClass("active");
+        $(".comments-container").addClass("hidden");
+        toShow.removeClass('hidden');
     });
+
+    // initialize magnific-popup
+    var $photos = $(".photo-group");
+    if ($photos.length) {
+        $photos.magnificPopup({
+            delegate: "a",
+            type: "image",
+            mainClass: 'mfp-with-fade',
+            preloader: true,
+            removalDelay: 300,
+            showCloseBtn: false,
+            gallery: {
+                enabled: true
+            }
+        });
+    }
 
 }(jQuery));
