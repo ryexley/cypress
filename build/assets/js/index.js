163cea2bb2d54afdca9089890f81813e099ab0a5
Bob Yexley <bob@yexley.net>
2015-06-12 12:26:58 -0400

    More tweaking of cards...gettin' there...like this a little better

--- build/assets/js/index.js
+++ build/assets/js/index.js
@@ -1,3 +1,6 @@
+// TODO: consider replacing parallax.js with a simple function similar to:
+// https://github.com/juanpablob/santiagum/blob/master/assets/js/santiagum.js#L15
+
 (function ($, global) {
 
     var app = global.Cypress = global.Cypress || {
